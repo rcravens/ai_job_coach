f84d6a3ef9344cfbb91218513873452b36767c54
rcravens <bob.cravens@gmail.com>
2024-03-20 09:01:15 -0500

    refactor - now can select the resume source to allow gpt-4 token limit to not be reached

--- Candidate.py
+++ Candidate.py
@@ -5,32 +5,52 @@ from PyPDF2 import PdfReader
 
 class Candidate:
     def __init__(self, candidate_name):
+        self._local_files = ['app_data.js', 'resume.txt', 'resume.pdf']
+        self._resume_source = None
+        self._resume_texts = dict()
+
         self.candidate_name = candidate_name
-        self.resume_source = ''
-        self.resume_text = ''
+        self.resume_sources = []
         self.sample_writing_style = ''
 
+        self._discover_resume_sources()
         self._read_resume()
         self._read_sample_writing()
 
-    def _read_resume(self):
-        self.resume_source = None
-        self.resume_text = ''
-        source_files = ['app_data.js', 'resume.txt', 'resume.pdf']
-        for source_file in source_files:
+    @property
+    def resume_source(self):
+        return self._resume_source
+
+    @resume_source.setter
+    def resume_source(self, source_file):
+        self._resume_source = source_file
+
+    @property
+    def resume_text(self):
+        return self._resume_texts[self._resume_source]
+
+    def _discover_resume_sources(self):
+        for source_file in self._local_files:
             if os.path.exists(source_file):
-                self.resume_source = source_file
-                if source_file.endswith('.pdf'):
-                    with open(source_file, 'rb') as f:
-                        pdf_reader = PdfReader(f)
-                        for page in pdf_reader.pages:
-                            self.resume_text += page.extract_text()
-                else:
-                    with open(source_file, 'r') as f:
-                        self.resume_text = f.readlines()
-                        self.resume_text = ''.join(self.resume_text)
-            if len(self.resume_text) > 0:
-                break
+                if self._resume_source is None:
+                    self._resume_source = source_file
+                self.resume_sources.append(source_file)
+
+    def _read_resume(self):
+        self._resume_texts = dict()
+        for source_file in self.resume_sources:
+            resume_text = ''
+            if source_file.endswith('.pdf'):
+                with open(source_file, 'rb') as f:
+                    pdf_reader = PdfReader(f)
+                    for page in pdf_reader.pages:
+                        resume_text += page.extract_text()
+            else:
+                with open(source_file, 'r') as f:
+                    resume_text = f.readlines()
+                    resume_text = ''.join(resume_text)
+
+            self._resume_texts[source_file] = resume_text
 
     def _read_sample_writing(self):
         self.sample_writing_style = ''
